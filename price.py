9326f9c7e6e5ecee5c7959a1d31069d7a30ea50d
Rick Seeger <rick.seeger@gmail.com>
2016-11-03 18:38:51 -0700

    wrap execution in def main()

--- price.py
+++ price.py
@@ -7,24 +7,30 @@ import sys, argparse, logging
 from btclib import logger, get_bitcoin_price
 
 
-# parse arguments
-parser = argparse.ArgumentParser(description='Fetch latest Bitcoin price')
-parser.add_argument('-u', '--USD', help='convert a USD amount to BTC', nargs=1, type=float, required=False)
-parser.add_argument('-v', '--verbose', help='show verbose output', action='store_true', required=False)
-args = vars(parser.parse_args())
+def main():
 
-if (args['verbose']):
-    logger.setLevel(logging.DEBUG)
-else:
-    logger.setLevel(logging.WARNING)
+    # parse arguments
+    parser = argparse.ArgumentParser(description='Fetch latest Bitcoin price')
+    parser.add_argument('-u', '--USD', help='convert a USD dollar amount to BTC', nargs=1, type=float, required=False)
+    parser.add_argument('-v', '--verbose', help='show verbose output', action='store_true', required=False)
+    args = vars(parser.parse_args())
+
+    if (args['verbose']):
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.WARNING)
 
 
-price = get_bitcoin_price()
+    price = get_bitcoin_price()
 
-if args['USD'] is None:
-    sys.stdout.write('${:,.2f} = 1 BTC\n'.format(price))
-else:
-    usd = args['USD'][0]
-    btc = usd / price
-    sys.stdout.write('${:,.2f} = {:,.8f} BTC\n'.format(usd, btc))
+    if args['USD'] is None:
+        sys.stdout.write('${:,.2f} = 1 BTC\n'.format(price))
 
+    else:
+        usd = args['USD'][0]
+        btc = usd / price
+        sys.stdout.write('${:,.2f} = {:,.8f} BTC\n'.format(usd, btc))
+
+
+if __name__ == "__main__":
+    main()
